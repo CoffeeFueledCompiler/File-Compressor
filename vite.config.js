13f368d589440b6d9a5ba3aed676053e2aa9c849
RajSharma-glitched <lucifer.vrs2019@gmail.com>
2024-03-02 21:59:41 +0530

    Patch-4

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['jszip'] 
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // Additional SCSS options if needed (e.g., includePaths)
+      }
     }
   }
 })
